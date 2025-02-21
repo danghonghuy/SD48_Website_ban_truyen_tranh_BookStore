e5e83d9750986b269695ae01f60f7d5bee6765f0
ThanhPH45763 <thanhnvph45763@fpt.edu.vn>
2025-02-21 14:30:22 +0700

    up

---
+++ backend_comic_service/src/test/java/com/example/backend_comic_service/BackendComicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_comic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendComicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
