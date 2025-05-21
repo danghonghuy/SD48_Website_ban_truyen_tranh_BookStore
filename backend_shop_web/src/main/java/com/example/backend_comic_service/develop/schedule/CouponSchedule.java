4d6a2484ad9f0ef77f57720f607c9b73dac4c6ff
ducvaph45821 <ducvaph45821@fpt.edu.vn>
2025-05-21 23:01:57 +0700

    úpchedule

--- backend_shop_web/src/main/java/com/example/backend_comic_service/develop/schedule/CouponSchedule.java
+++ backend_shop_web/src/main/java/com/example/backend_comic_service/develop/schedule/CouponSchedule.java
@@ -1,6 +1,7 @@
 package com.example.backend_comic_service.develop.schedule;
 
 import com.example.backend_comic_service.develop.constants.CouponStatusEnum;
+import com.example.backend_comic_service.develop.constants.DiscountStatusEnum;
 import com.example.backend_comic_service.develop.repository.CouponRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +16,17 @@ public class CouponSchedule {
     public CouponSchedule(CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
     }
-    @Scheduled(cron = "0 59 23 * * ?")
+    @Scheduled(cron = "0 0 0 * * ?")
     public void resetCouponOutDate(){
         try{
-            System.out.println("--- Staring schedule ---");
-            couponRepository.resetCouponProgram(CouponStatusEnum.COUPON_STATUS_EXPIRE);
-            System.out.println("--- End schedule ---");
+            log.info("--- Staring resetCouponOutDate ---");
+            couponRepository.resetCouponProgram(CouponStatusEnum.COUPON_STATUS_EXPIRED);
+            couponRepository.imminentCouponProgram(CouponStatusEnum.COUPON_STATUS_IMMINENT);
+            couponRepository.activeCouponProgram(CouponStatusEnum.COUPON_STATUS_ACTIVE);
+            log.info("--- End resetCouponOutDate ---");
         }
         catch (Exception e){
-            log.error(e.getMessage());
-            System.out.println("--- Schedule ending with fail error: " + e.getMessage() + " ---");
+            log.error("--- Schedule resetCouponOutDate error: " + e.getMessage() + " ---");
         }
     }
 }
