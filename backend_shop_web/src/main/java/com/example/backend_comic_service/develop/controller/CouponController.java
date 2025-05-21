6e58ff3a96fb38f5297da7a5ad6e281f120484c7
ducvaph45821 <ducvaph45821@fpt.edu.vn>
2025-05-21 23:00:03 +0700

    upcontroler

--- backend_shop_web/src/main/java/com/example/backend_comic_service/develop/controller/CouponController.java
+++ backend_shop_web/src/main/java/com/example/backend_comic_service/develop/controller/CouponController.java
@@ -11,6 +11,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -24,11 +26,13 @@ public class CouponController {
     public BaseResponseModel<CouponModel> addOrChange(@RequestBody CouponRequest request) {
         return couponService.addOrChange(request);
     }
+
     @GetMapping("/delete")
-    public BaseResponseModel<Integer> changeStatus(@RequestParam(value = "id", required = false)  Integer id,
-                                                   @RequestParam(value = "status", required = false)  Integer status) {
-        return couponService.delete(id,status);
+    public BaseResponseModel<Integer> changeStatus(@RequestParam(value = "id", required = false) Integer id,
+                                                   @RequestParam(value = "status", required = false) Integer status) {
+        return couponService.delete(id, status);
     }
+
     @GetMapping("/detail/{id}")
     public BaseResponseModel<CouponModel> detail(@PathVariable Integer id) {
         return couponService.getCouponById(id);
@@ -40,21 +44,23 @@ public class CouponController {
     }
 
     @GetMapping("/get-list-coupon")
-    public BaseListResponseModel<List<CouponModel>> getListDiscount(@RequestParam(value = "startDate", required = false) Date startDate,
-                                                                      @RequestParam(value = "endDate", required = false)Date endDate,
-                                                                      @RequestParam(value = "minValue", required = false) Integer minValue,
-                                                                      @RequestParam(value = "maxValue", required = false) Integer maxValue,
-                                                                      @RequestParam(value = "keySearch", required = false) String keySearch,
-                                                                      @RequestParam(value = "status", required = false) Integer status,
-                                                                      @RequestParam(value = "pageIndex") Integer pageIndex,
-                                                                      @RequestParam(value = "pageSize") Integer pageSize) {
+    public BaseListResponseModel<List<CouponModel>> getListDiscount(@RequestParam(value = "startDate", required = false) LocalDateTime startDate,
+                                                                    @RequestParam(value = "endDate", required = false) LocalDateTime endDate,
+                                                                    @RequestParam(value = "minValue", required = false) Integer minValue,
+                                                                    @RequestParam(value = "maxValue", required = false) Integer maxValue,
+                                                                    @RequestParam(value = "keySearch", required = false) String keySearch,
+                                                                    @RequestParam(value = "status", required = false) Integer status,
+                                                                    @RequestParam(value = "pageIndex") Integer pageIndex,
+                                                                    @RequestParam(value = "pageSize") Integer pageSize) {
         Pageable pageable = PageRequest.of(pageIndex - 1, pageSize);
         return couponService.getListCoupon(startDate, endDate, minValue, maxValue, keySearch, status, pageable);
     }
+
     @GetMapping("/generate-code")
     public BaseResponseModel<String> generateCode() {
         return couponService.generateCouponCode();
     }
+
     @GetMapping("/get-coupon-code")
     public BaseResponseModel<Double> getCoupon(@RequestParam(value = "code", required = false) String code,
                                                @RequestParam(value = "sumPrice", required = false) Double sumPrice) {
